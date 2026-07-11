d61b77b33574142f2dadfd800c6e2f756909ea6b
Abdul Haadi <haadikkd24@gmail.com>
2026-07-11 16:26:39 +0530

    RBAC implementation

--- core/views.py
+++ core/views.py
@@ -3,29 +3,36 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 
+from .permissions import IsEmployer, IsCandidate, IsAdmin
 from .models import Job
 from .serializers import JobSerializer
 
+
 class JobListAPIView(APIView):
     permission_classes = [IsAuthenticated]
+
     def get(self, request):
         jobs = Job.objects.all()
-        serializer = JobSerializer(jobs, many = True)
+        serializer = JobSerializer(jobs, many=True)
         return Response(serializer.data)
 
+
 class JobCreateAPIView(APIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsEmployer]
+
     def post(self, request):
-        serializer = JobSerializer(data= request.data)
+        serializer = JobSerializer(data=request.data)
 
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
-    
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
 class UserTestAPIView(APIView):
     permission_classes = [IsAuthenticated]
+
     def get(self, request):
         return Response({
             "message": "User API Working"
